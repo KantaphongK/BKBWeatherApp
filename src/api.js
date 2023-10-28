4d1fe662b252ae78e266a0be54835ee1b18b40ba
KIKantaphong <k.kantaphong@kkumail.com>
2023-10-28 19:40:58 +0700

    feat:added all components

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8f527c9039msh6b477119096b2a6p155e87jsn376671af45db",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL =
+  "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "aee6164890661722cfe9382a7c475a38";
